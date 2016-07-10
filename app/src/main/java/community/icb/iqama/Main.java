e4882e63892aed80b910610343e0d89bdac23d6c
AmrAbed <amraabed@gmail.com>
2016-07-10 02:41:14 -0400

    Add support links

--- app/src/main/java/community/icb/iqama/Main.java
+++ app/src/main/java/community/icb/iqama/Main.java
@@ -1,11 +1,17 @@
 package community.icb.iqama;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.View;
 
 import community.icb.iqama.utilities.Date;
 
@@ -15,50 +21,59 @@ import community.icb.iqama.utilities.Date;
  * @author AmrAbed
  */
 public class Main extends FragmentActivity
+		implements View.OnClickListener, DialogInterface.OnClickListener
 {
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.main);
+		setContentView(R.layout.main);
 
-        ((ViewPager) findViewById(R.id.pager)).setAdapter(new Adapter(getSupportFragmentManager()));
+		((ViewPager) findViewById(R.id.pager)).setAdapter(new Adapter(getSupportFragmentManager()));
+	}
 
-    }
+	@Override
+	public void onClick(View v)
+	{
+		new AlertDialog.Builder(this).setTitle(R.string.support).setItems(R.array.support, this)
+				.create().show();
+	}
 
-    private class Adapter extends FragmentPagerAdapter
-    {
+	private class Adapter extends FragmentPagerAdapter
+	{
 
-        public Adapter(FragmentManager manager)
-        {
-            super(manager);
-        }
+		public Adapter(FragmentManager manager)
+		{
+			super(manager);
+		}
 
-        @Override
-        public Fragment getItem(int position)
-        {
-            return Section.newInstance(position);
-        }
+		@Override
+		public Fragment getItem(int position)
+		{
+			return Section.newInstance(position);
+		}
 
-        @Override
-        public int getCount()
-        {
-            return 2;
-        }
+		@Override
+		public int getCount()
+		{
+			return 100;
+		}
 
-        @Override
-        public CharSequence getPageTitle(int position)
-        {
-            switch(position)
-            {
-                case 0:
-                    return Date.today().toString(Date.DEFAULT_FORMAT);
-                case 1:
-                    return Date.tomorrow().toString(Date.DEFAULT_FORMAT);
-            }
-            return super.getPageTitle(position);
-        }
-    }
+		@Override
+		public CharSequence getPageTitle(int position)
+		{
+			return Date.today().plusDays(position).toString(Date.DEFAULT_FORMAT);
+		}
+	}
+
+	@Override
+	public void onClick(DialogInterface dialog, int i)
+	{
+		final String[] ids = {"5T6WN83XVX3BC", "N46UMV92GCUD2", "Y75AT2XMDNZ3G"};
+		final Uri uri = Uri
+				.parse("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=" + ids[i]);
+		startActivity(new Intent(Intent.ACTION_VIEW, uri));
+	}
 
 }
